a2eed3351480cb453c1bd8b7bf8936fb0ed146fb
abzaver <abzaver@gmail.com>
2023-02-17 16:58:58 +0300

    some little changes

--- pubBot.py
+++ pubBot.py
@@ -29,10 +29,10 @@ except ImportError:
 
 if __version_info__ < (20, 0, 0, "alpha", 1):
     raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
         f"{TG_VER} version of this example, "
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
-    f"This example is not compatible with your current PTB version {TG_VER}. To view the "
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
