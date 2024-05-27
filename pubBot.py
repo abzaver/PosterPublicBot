28f50fc18a76168074d091ff57dbe7ccd0f9b284
abzaver <abzaver@gmail.com>
2024-05-26 21:25:36 -0400

    experiment with reactions

--- pubBot.py
+++ pubBot.py
@@ -14,13 +14,15 @@ Basic Echobot example, repeats messages.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
+from telegram.constants import ReactionEmoji
+
 import hamming_db as ham_dist
 import datetime
 
 import logging
 import os
 
-from telegram import __version__ as TG_VER, MessageEntity
+from telegram import __version__ as TG_VER, MessageEntity, ReactionTypeEmoji
 
 try:
     from telegram import __version_info__
@@ -33,8 +35,8 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
         f"{TG_VER} version of this example, "
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
-from telegram import ForceReply, Update, Chat
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, MessageReactionHandler
 
 # Enable logging
 logging.basicConfig(
@@ -49,104 +51,74 @@ token = os.getenv("TLG_TOKEN")
 # context.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
-    if update.effective_chat.type == Chat.CHANNEL:
-        return
     user = update.effective_user
     await update.message.reply_html(
         rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=False),
+        reply_markup=ForceReply(selective=True),
     )
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    if update.effective_chat.type == Chat.CHANNEL:
-        return
     await update.message.reply_text("Help!")
 
 
-async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """reply the user message, but only if bot mentioned"""
-    if update.effective_chat.type == Chat.CHANNEL:
-        return
-    message_entities = update.effective_message.parse_entities(
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    #await update.message.reply_text(update.message.text)
+    #await update.message.set_reaction(reaction=ReactionTypeEmoji(ReactionEmoji.THUMBS_UP))
+    #"""Echo the user message."""
+    #return
+
+    #https://gist.github.com/nafiesl/4ad622f344cd1dc3bb1ecbe468ff9f8a
+    user_entities = update.message.parse_entities(
         [MessageEntity.MENTION, MessageEntity.TEXT_MENTION]
     )
-    if context.bot.name in message_entities.values():
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Привет, {update.effective_user.username}! Чего хотел?',
-                                       reply_to_message_id=update.effective_message.message_id)
+    if context.bot.name in user_entities.values():
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text,
+                                       reply_to_message_id=update.message.message_id)
     #print("message id:", update.message.forward_from_message_id, " chat_id:", update.message.forward_from_chat.id)
 
-async def process_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """trying to process the received photo"""
-    #await context.bot.send_message(chat_id=update.effective_chat.id, text="I see photo in this message!", reply_to_message_id=update.message.message_id)
-    file_id = update.effective_message.photo[-1].file_id
-    new_file = await context.bot.get_file(file_id)
-    await new_file.download_to_drive('./data/img_to_search')
-    ham_db_conn = ham_dist.create_or_open_db(ham_dist.DATABASE_NAME)
-    search_results = ham_dist.search_by_image(ham_db_conn, './data/img_to_search', 10)
-    if search_results:
-        reply_msg = f'Чувак, это \N{accordion}! '
-        for result in search_results:
-            if result[1] == 1242081849:  # если чат это "прогрессивные мемы"
-                chat = await context.bot.get_chat('@progressive_memes')
-                reply_msg += f'Было здесь: t.me/progressive_memes/{result[3]}. '
-            else:
-                reply_msg += f' Было в чате {result[1]}, примерно {datetime.datetime.fromtimestamp(result[4])}. '
-
-                #await context.bot.send_message(chat_id=update.effective_chat.id, text='Я видел это в других чатах, но не могу сказать точно в каком сообщении',
-                #                               reply_to_message_id=update.message.message_id)
-                #chat = await context.bot.get_chat('@progressive_memes')
-                #logger.info(chat)
-                #await context.bot.forward_message(chat_id=update.effective_chat.id, from_chat_id=result[1], message_id=result[3])
-                #await context.bot.send_message(chat_id=update.effective_chat.id,
-                #                               text=f"Чувак, это \N{accordion}! message id: {result[3]} from chat id: {result[1]}, date {datetime.datetime.fromtimestamp(result[4])}",
-                #                               reply_to_message_id=update.message.message_id)
-    else:
-        reply_msg = ('Ух ты! Что-то новенькое!')
-        imgs_tuple_list = [tuple((update.effective_chat.id, str('./data/img_to_search'), update.effective_message.message_id,
-                                  int(update.effective_message.date.timestamp()), ''))]
-        ham_dist.add_images_by_path(ham_db_conn, imgs_tuple_list)
-        ham_dist.add_chat_to_db(ham_db_conn, [update.effective_chat.id, update.effective_chat.title, update.effective_chat.type])
-    ham_db_conn.close()
-    if update.effective_chat.type != Chat.CHANNEL:
-        await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text=reply_msg,
-                                       reply_to_message_id=update.effective_message.message_id)
-
-
-async def process_animation(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """trying to process the received animation"""
-    file_name = './data/animation_to_search'
-    file_id = update.effective_message.effective_attachment.file_id
-
-    try:
+    if update.message.photo:
+        #await context.bot.send_message(chat_id=update.effective_chat.id, text="I see photo in this message!", reply_to_message_id=update.message.message_id)
+        file_id = update.message.photo[-1].file_id
         new_file = await context.bot.get_file(file_id)
-        await new_file.download_to_drive(file_name)
-    except BaseException as e:
-        logger.info(e)
-        if update.effective_chat.type != Chat.CHANNEL:
-            await update.effective_message.reply_text('Слишком большой файл для меня... \N{Neutral face}')
-        return
+        await new_file.download_to_drive('img_to_search')
+        ham_db_conn = ham_dist.create_or_open_db(ham_dist.DATABASE_NAME)
+        search_results = ham_dist.search_by_image(ham_db_conn, 'img_to_search', 10)
+        if search_results:
+            for result in search_results:
+                if result[1] == 1242081849:  # если чат это "прогрессивные мемы"
+                    chat = await context.bot.get_chat('@progressive_memes')
+                    link_to_message = f'Чувак, это \N{accordion}! Было здесь: t.me/progressive_memes/{result[3]}'
+                    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                                   text=link_to_message,
+                                                   reply_to_message_id=update.message.message_id)
+                    #await context.bot.forward_message(chat_id=update.effective_chat.id, from_chat_id=chat.id, message_id={result[3]})
+                else:
+                    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                                   text='Я видел это в других чатах, но не могу сказать точно в каком сообщении',
+                                                   reply_to_message_id=update.message.message_id)
+                    #chat = await context.bot.get_chat('@progressive_memes')
+                    #logger.info(chat)
+                    #await context.bot.forward_message(chat_id=update.effective_chat.id, from_chat_id=result[1], message_id=result[3])
+                    #await context.bot.send_message(chat_id=update.effective_chat.id,
+                    #                               text=f"Чувак, это \N{accordion}! message id: {result[3]} from chat id: {result[1]}, date {datetime.datetime.fromtimestamp(result[4])}",
+                    #                               reply_to_message_id=update.message.message_id)
+        else:
+            await update.message.reply_text('Ух ты! Что-то новенькое!')
+        ham_db_conn.close()
 
-    ham_db_conn = ham_dist.create_or_open_db(ham_dist.DATABASE_NAME)
-    hash_img_to_search = ham_dist.animation_to_hash(file_name)
-    search_results = ham_dist.search_by_image_hash(ham_db_conn, hash_img_to_search, 10)
-    if search_results:
-        reply_msg = f"Чувак, это \N{accordion}!"
-        for result in search_results:
-            reply_msg += f" Было в чате {result[1]}, примерно {datetime.datetime.fromtimestamp(result[4])}. "
+
+async def reaction_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg_reaction = update.message_reaction
+    if len(msg_reaction.new_reaction) != 0 and len(msg_reaction.old_reaction) == 0:
+        logger.info("This is new reaction %s", msg_reaction.new_reaction[0].emoji)
+    elif len(msg_reaction.new_reaction) != 0 and len(msg_reaction.old_reaction) != 0:
+        logger.info("Reaction is changed from %s, to %s", msg_reaction.old_reaction[0].emoji,
+                    msg_reaction.new_reaction[0].emoji)
     else:
-        reply_msg = 'Ух ты! Что-то новенькое!'
-        imgs_tuple_list = [tuple((update.effective_chat.id, str(hash_img_to_search), update.effective_message.message_id,
-                                  int(update.effective_message.date.timestamp()), ''))]
-        ham_dist.add_images_by_hash(ham_db_conn, imgs_tuple_list)
-        ham_dist.add_chat_to_db(ham_db_conn, [update.effective_chat.id, update.effective_chat.title, update.effective_chat.type])
-    ham_db_conn.close()
-    if update.effective_chat.type != Chat.CHANNEL:
-        await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text=reply_msg,
-                                       reply_to_message_id=update.effective_message.message_id)
+        logger.info("Reaction removed: %s", msg_reaction.message_id)
 
 
 def main() -> None:
@@ -159,13 +131,11 @@ def main() -> None:
     application.add_handler(CommandHandler("help", help_command))
 
     # on non command i.e message - echo the message on Telegram
-
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
-    application.add_handler(MessageHandler(filters.PHOTO & ~filters.COMMAND, process_photo))
-    application.add_handler(MessageHandler((filters.Document.GIF | filters.Document.MP4 | filters.VIDEO) & ~filters.COMMAND, process_animation))
+    application.add_handler(MessageHandler(filters.TEXT | filters.PHOTO & ~filters.COMMAND, echo))
+    application.add_handler(MessageReactionHandler(reaction_handler))
 
     # Run the bot until the user presses Ctrl-C
-    application.run_polling()
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 if __name__ == "__main__":
